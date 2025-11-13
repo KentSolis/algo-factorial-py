283dd835625c2c5c58ef2cf9da3371abbff9d540
Kent Solis <kentsolis77@gmail.com>
2025-11-12 19:28:08 -0700

    first solution commit for the python factorial practice assignment

--- factorial.py
+++ factorial.py
@@ -1,3 +1,8 @@
 def factorial(num):
-	# your code here
-	pass
+	# we want to start a count up multipying each number to n, n! ex 6! = nx6x5x4x3x2x1
+    # this can look like 1x2x3x4x5x6xn just the same as nx6x5x4x3x2x1
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+    return result
+    
